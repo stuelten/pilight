b07d95d68e85880c6a99d38d4803a869f09334ca
Timo Stülten <timo@stuelten.de>
2014-10-27 16:38:29 +0100

    Setup script hinzu

--- bin/pilight-setup-raspi.sh
+++ bin/pilight-setup-raspi.sh
@@ -7,7 +7,7 @@
 sudo apt-get install -y screen nano mc wget bash-completion git
 
 # get maven
-wget http://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+wget http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
 tar -xvzf apache-maven-3.2.3-bin.tar.gz
 mkdir $HOME/bin
 ln -s apache-maven-3.2.3/bin/mvn $HOME/bin/mvn
@@ -15,3 +15,6 @@ ln -s apache-maven-3.2.3/bin/mvn $HOME/bin/mvn
 # get sources
 git clone timo@schatzkammer.subluna.org:Dev/git
 mv git pilight
+
+# aliase einrichten
+echo "alias l='ls -al $@'" >> $HOME/.bash_rc
