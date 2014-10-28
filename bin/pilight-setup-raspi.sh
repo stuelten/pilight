fc6dbb3d29da3674cee97206eb6879c72cab8820
Timo Stülten <timo@stuelten.de>
2014-10-28 12:44:01 +0100

    Add proceed function

--- bin/pilight-setup-raspi.sh
+++ bin/pilight-setup-raspi.sh
@@ -16,46 +16,60 @@ ask_proceed_step() {
 
 }
 
+wait_for_enter() {
+    read -p "Press Enter to proceed..." -er dummy
+}
+
 if ( ask_proceed_step "Create individual unique ssh keys for this host" )
 then
     sudo dpkg-reconfigure openssh-server
-    read -p "Press Enter" -er dummy
 fi
 
-echo "Configure WLAN network:"
-read -p "Enter WLAN SSID: " -er ssid
-read -p "Enter WLAN passphrase: " -er passphrase
+if ( ask_proceed_step "Configure WLAN network" )
+then
+    read -p "Enter WLAN SSID: " -er ssid
+    read -p "Enter WLAN passphrase: " -er passphrase
 
-echo "Create standard interface config..."
-cat interfaces | sed "s^#SSID#^$ssid^g" | sed "s^#PASSPHRASE#^$passphrase^g" > /etc/network/interface
-echo "New network config:"
-echo "-------------------"
-cat /etc/network/interface
-read -p "Press Enter" -er dummy
+    echo "Create standard interface config..."
+    cat interfaces | sed "s^#SSID#^$ssid^g" | sed "s^#PASSPHRASE#^$passphrase^g" > /etc/network/interface
+    echo "New network config:"
+    echo "-------------------"
+    cat /etc/network/interface
 
-echo "Restart network services to join WLAN..."
-sudo service networking restart
-read -p "Press Enter" -er dummy
+    echo "Restart network services to join WLAN..."
+    sudo service networking restart
+    if ( ping -A -c 3 -w 5 www.heise.de )
+    then
+        echo "Network setup successful"
+    else
+        echo "Error accessing network. Press Ctrl+C to abort"
+        wait_for_enter
+    fi
+fi
 
-echo "Install some basic packages..."
-sudo apt-get install -y nano git screen mc wget bash-completion
-read -p "Press Enter" -er dummy
+if ( ask_proceed_step "Install basic packages (nano git screen mc wget bash-completion)" )
+then
+    sudo apt-get install -y nano git screen mc wget bash-completion
+fi
 
-echo "Install maven..."
-wget http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
-tar -xvzf apache-maven-3.2.3-bin.tar.gz
-mkdir $HOME/bin
-ln -s apache-maven-3.2.3/bin/mvn $HOME/bin/mvn
-read -p "Press Enter" -er dummy
+if ( ask_proceed_step "Install maven" )
+then
+    wget http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+    tar -xvzf apache-maven-3.2.3-bin.tar.gz
+    mkdir $HOME/bin
+    ln -s apache-maven-3.2.3/bin/mvn $HOME/bin/mvn
+fi
 
-echo "Get sources for pilight..."
-git clone timo@schatzkammer.subluna.org:Dev/git
-mv git pilight
-read -p "Press Enter" -er dummy
+if ( ask_proceed_step "Get sources for pilight" )
+then
+    git clone timo@schatzkammer.subluna.org:Dev/git
+    mv git pilight
+fi
 
-echo "Configure bash aliases..."
-echo "alias l='ls -al $@'" >> $HOME/.bash_rc
-echo "New .bash_rc:"
-echo "-------------"
-cat $HOME/.bash_rc
-read -p "Press Enter" -er dummy
+if ( ask_proceed_step "Configure bash aliases" )
+then
+    echo "alias l='ls -al $@'" >> $HOME/.bash_rc
+    echo "New .bash_rc:"
+    echo "-------------"
+    cat $HOME/.bash_rc
+fi
