17bdcc06378040840ff5c6c1354a689f66812135
Timo Stülten <timo@stuelten.de>
2014-10-28 11:15:16 +0000

    Add output

--- bin/pilight-setup-raspi.sh
+++ bin/pilight-setup-raspi.sh
@@ -3,39 +3,44 @@
 # Installs all necessary software/packages and configuration on rasperian
 #
 
-# create individual unique ssh keys for this host
+echo "Create individual unique ssh keys for this host..."
 sudo dpkg-reconfigure openssh-server
 read -p "Press Enter" -er dummy
 
-# install WLAN
+echo "Configure WLAN network:"
 read -p "Enter WLAN SSID: " -er ssid
 read -p "Enter WLAN passphrase: " -er passphrase
 
-# create standard interface config
-cat interfaces | sed "s°#SSID#°$ssid" | sed "s°#PASSPHRASE#°$passphrase" > /etc/network/interface
+echo "Create standard interface config..."
+cat interfaces | sed "s^#SSID#^$ssid^g" | sed "s^#PASSPHRASE#^$passphrase^g" > /etc/network/interface
+echo "New network config:"
+echo "-------------------"
 cat /etc/network/interface
 read -p "Press Enter" -er dummy
 
-# restart network services to join wlan
+echo "Restart network services to join WLAN..."
 sudo service networking restart
 read -p "Press Enter" -er dummy
 
-# install some basic packages
-sudo apt-get install -y screen nano mc wget bash-completion git
+echo "Install some basic packages..."
+sudo apt-get install -y nano git screen mc wget bash-completion
 read -p "Press Enter" -er dummy
 
-# get maven
+echo "Install maven..."
 wget http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
 tar -xvzf apache-maven-3.2.3-bin.tar.gz
 mkdir $HOME/bin
 ln -s apache-maven-3.2.3/bin/mvn $HOME/bin/mvn
 read -p "Press Enter" -er dummy
 
-# get sources
+echo "Get sources for pilight..."
 git clone timo@schatzkammer.subluna.org:Dev/git
 mv git pilight
 read -p "Press Enter" -er dummy
 
-# setup aliase
+echo "Configure bash aliases..."
 echo "alias l='ls -al $@'" >> $HOME/.bash_rc
+echo "New .bash_rc:"
+echo "-------------"
+cat $HOME/.bash_rc
 read -p "Press Enter" -er dummy
