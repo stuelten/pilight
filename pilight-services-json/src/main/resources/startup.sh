347c29f33b94f7b8a627bbf7aaed9414e1c30729
Timo Stülten <timo@stuelten.de>
2015-10-08 02:14:51 +0200

    modified startup script

--- pilight-services-json/src/main/resources/startup.sh
+++ pilight-services-json/src/main/resources/startup.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # Starts Restful service in embedded grizzly server
-java \
- -cp .:../pilight-services-json-*-jar-with-dependencies.jar \
- de.ckc.agwa.pilight.services.json.PiLightRestfulServiceMain
+java -cp ../pilight-services-json-*-jar-with-dependencies.jar de.ckc.agwa.pilight.services.json.PiLightRestfulServiceMain
