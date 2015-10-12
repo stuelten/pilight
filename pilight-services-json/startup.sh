86918043014c37e4559d79f75056dfc2028bde63
Timo Stülten <timo@stuelten.de>
2015-10-12 10:58:15 +0200

    added adhoc startup script

---
+++ pilight-services-json/startup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Starts Restful service in embedded grizzly server
+java -cp target/pilight-services-json-*-jar-with-dependencies.jar de.ckc.agwa.pilight.services.json.PiLightRestfulServiceMain
